525934992474df5d3e5a3ea4377e0cc2594c9788
大石海 <ishibasi48@gmail.com>
2020-01-13 15:57:06 +0900

    PinBallプロジェクト
    課題提出用です。

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+
+    int score = 0; //スコアの変数を定義
+    GameObject scoreText;
+
+    void Start () {
+        scoreText = GameObject.Find("ScoreText");
+    }
+
+    void Update () {
+        
+    }
+    // 衝突時の関数
+    void OnCollisionEnter(Collision collision) {
+        string collisionTag = collision.gameObject.tag;
+        int tempScore = score;
+        switch(collisionTag){
+            case "SmallStarTag":
+                this.score += 5;
+                break;
+            case "LargeStarTag":
+                this.score += 30;
+                break;
+            case "SmallCloudTag":
+                this.score += 20;
+                break;
+            case "LargeCloudTag":
+                this.score += 8000;
+                break;
+        }
+        if(tempScore != score){
+            scoreText.GetComponent<Text> ().text = "Score " + this.score;
+        }
+    }
+}
